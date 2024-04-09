4ce2eb4bfbd3afbeb6e75214949a09c4b7319291
Mohit Namdev <namdevmohit0@gmail.com>
2024-04-09 23:04:34 +0530

    few changes on checkout page

--- MedFlip Mini Project/medflip-frontend/src/components/Checkout.jsx
+++ MedFlip Mini Project/medflip-frontend/src/components/Checkout.jsx
@@ -1,33 +1,29 @@
 import React from 'react';
 
 function Checkout() {
-  // Retrieve cart items from sessionStorage
+ 
   const cartItems = JSON.parse(sessionStorage.getItem('cart')) || [];
-  
-  // Calculate total amount of all medicine prices
+
   const totalAmount = cartItems.reduce((total, item) => total + item.price, 0);
   
-  // Calculate shipping amount (assuming it's fixed at Rs. 50)
   const shippingAmount = 50;
   
-  // Calculate GST (18% of total amount)
+
   const gst = totalAmount * 0.18;
   
-  // Calculate grand total (total amount + shipping amount + GST)
+  
   const grandTotal = totalAmount + shippingAmount + gst;
   
-  // Function to remove an item from the cart
+  
   const removeFromCart = (index) => {
     const updatedCart = [...cartItems];
-    updatedCart.splice(index, 1); // Remove item at the specified index
-    sessionStorage.setItem('cart', JSON.stringify(updatedCart)); // Update sessionStorage with the modified cart
-    window.location.reload(); // Refresh the page to reflect the changes
+    updatedCart.splice(index, 1); 
+    sessionStorage.setItem('cart', JSON.stringify(updatedCart));
+    window.location.reload(); 
   };
-
-  // Function to clear the entire cart
   const clearCart = () => {
-    sessionStorage.removeItem('cart'); // Remove cart data from sessionStorage
-    window.location.reload(); // Refresh the page to reflect the changes
+    sessionStorage.removeItem('cart');
+    window.location.reload(); 
   };
 
   return (
@@ -42,7 +38,7 @@ function Checkout() {
               <p>Expiration Date: {item.expirationDate}</p>
               <p>Quantity: {item.quantity}</p>
               <p>Price: {item.price}</p>
-              <button onClick={() => removeFromCart(index)}>Remove</button> {/* Button to remove the item */}
+              <button onClick={() => removeFromCart(index)}>Remove</button> 
             </li>
           ))}
         </ul>
@@ -54,9 +50,9 @@ function Checkout() {
         <p>GST: {gst}</p>
         <p>Grand Total: {grandTotal}</p>
       </div>
-      <button onClick={clearCart}>Clear Cart</button> {/* Button to clear the entire cart */}
-      <button>Proceed to Payment</button> {/* Button to proceed with payment */}
-      {/* Add checkout form or further checkout steps here */}
+      <button onClick={clearCart}>Clear Cart</button>
+      <button>Proceed to Payment</button> 
+      
     </div>
   );
 }
