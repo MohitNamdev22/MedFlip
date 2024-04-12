e211e34f0694db89fc4d42cd279051eba49e4c8c
Mohit Namdev <namdevmohit0@gmail.com>
2024-04-12 11:27:38 +0530

    added footer

--- MedFlip Mini Project/medflip-frontend/src/App.jsx
+++ MedFlip Mini Project/medflip-frontend/src/App.jsx
@@ -14,24 +14,29 @@ function App() {
   return (
     <RecoilRoot>
       <CartProvider>
-      <div style={{ width: "100vW", height: "100vh", backgroundColor: "rgb(233 233 233);" }}>
-        <Router>
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<MedicationList/>}/>
-            <Route path="/admin/medication" element={<AdminDashboard />} />
-            <Route path="/auth/login" element={<LoginForm />} />
-            <Route path="/checkout" element={<Checkout />} />
-            <Route path="/auth/register" element={<RegistrationForm />} />
-            <Route path="/payment" element={<Payment />} />
-          </Routes>
-        </Router>
-      </div>
+        <div style={{ width: "100vW", height: "100vh", backgroundColor: "rgb(233 233 233);" }}>
+          <Router>
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<MedicationList/>}/>
+              <Route path="/admin/medication" element={<AdminDashboard />} />
+              <Route path="/auth/login" element={<LoginForm />} />
+              <Route path="/checkout" element={<Checkout />} />
+              <Route path="/auth/register" element={<RegistrationForm />} />
+              <Route path="/payment" element={<Payment />} />
+            </Routes>
+          </Router>
+          {/* Copyright and developer statement */}
+          <div style={{ textAlign: "center", padding: "5px 0px", height:"50px", backgroundColor: "#333", color: "#fff" }}>
+          © 2024 All rights reserved<br></br>Designed & Developed by{' '}
+          <a href="https://github.com/MohitNamdev22" style={{ fontWeight: "bold", color: "#fff", textDecoration: "none" }}>
+              Mohit Namdev
+            </a>
+          </div>
+        </div>
       </CartProvider>
     </RecoilRoot>
   );
 }
 
 export default App;
-
-
